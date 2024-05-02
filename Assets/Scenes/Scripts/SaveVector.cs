574669710f8cbe3feae99f225ee32465199cd7d9
Kristian <krestrepoo@eafit.edu.co>
2024-05-02 18:03:25 -0500

    feat: cross product

--- Assets/Scenes/Scripts/SaveVector.cs
+++ Assets/Scenes/Scripts/SaveVector.cs
@@ -8,9 +8,6 @@ public class SaveVector : MonoBehaviour
     public TMP_InputField inputFieldInitialX;
     public TMP_InputField inputFieldInitialY;
     public TMP_InputField inputFieldInitialZ;
-    public TMP_InputField inputFieldFinalX;
-    public TMP_InputField inputFieldFinalY;
-    public TMP_InputField inputFieldFinalZ;
 
     public int FlagVector;
     public Button button;
@@ -24,17 +21,16 @@ public class SaveVector : MonoBehaviour
     public void CreateVector()
     {
 
-        Vector3 initialPoint = new Vector3(float.Parse(inputFieldInitialX.text), float.Parse(inputFieldInitialY.text), float.Parse(inputFieldInitialZ.text));
-        Vector3 finalPoint = new Vector3(float.Parse(inputFieldFinalX.text), float.Parse(inputFieldFinalY.text), float.Parse(inputFieldFinalZ.text));
+        Vector3 vector = new Vector3(float.Parse(inputFieldInitialX.text), float.Parse(inputFieldInitialY.text), float.Parse(inputFieldInitialZ.text));
 
         if (FlagVector==1){
-            GameManager.instance.v1 = finalPoint-initialPoint;
+            GameManager.instance.v1 = vector;
         }
         else if (FlagVector==2){
-            GameManager.instance.v2 = finalPoint-initialPoint;
+            GameManager.instance.v2 = vector;
         }
         else{
-            GameManager.instance.v3 = finalPoint-initialPoint;
+            GameManager.instance.v3 = vector;
         }
         
     }
--- Assets/Scenes/Scripts/VectorRederer.cs
+++ Assets/Scenes/Scripts/VectorRederer.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class VectorRenderer : MonoBehaviour
 {
+    public Transform vector1;
+    public Transform vector2;
+    public Transform vector3;
 
     void Start()
     {
-        print(GameManager.instance.v1);
-        print(GameManager.instance.v2);
-        print(GameManager.instance.v3);
+        GameManager.instance.v3 = Vector3.Cross(GameManager.instance.v1, GameManager.instance.v2);
+        print(vector1.position);
+        print(vector2.position);
+        print(vector3.position);
+
     }
 }
