db4b3e97141a4fc48d1e21736e31631f585f3361
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2024-05-02 20:54:22 -0500

    refactor: Scale Used and Camera Position Modified

--- Assets/Scenes/Scripts/VectorRederer.cs
+++ Assets/Scenes/Scripts/VectorRederer.cs
@@ -20,13 +20,35 @@ public class VectorRenderer : MonoBehaviour
         float magnitudev1 = GameManager.instance.v1.magnitude;
         float magnitudev2 = GameManager.instance.v2.magnitude;
         float magnitudev3 = GameManager.instance.v3.magnitude;
+        print(GameManager.instance.v1);
+        print(GameManager.instance.v2);
+        print(GameManager.instance.v3);
         print(magnitudev1);
         print(magnitudev2);
         print(magnitudev3);
-        print(GameManager.instance.v3);
+        if (magnitudev3 > 20){
+            magnitudev3 = 20;
+        }
+        if (magnitudev2 > 20){
+            if (magnitudev1 > magnitudev2){
+                magnitudev2 = 10;
+            } else {
+                magnitudev2 = 15;
+            }
+        }
+        if (magnitudev1 > 20){
+            if (magnitudev2 > magnitudev1){
+                magnitudev1 = 10;
+            } else {
+                magnitudev1 = 15;
+            }
+        }
+        print(magnitudev1);
+        print(magnitudev2);
+        print(magnitudev3);
 
-        vector1.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev1/20);
-        vector2.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev2/20);
-        vector3.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev3/20);
+        vector1.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev1/20*(float)0.5);
+        vector2.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev2/20*(float)0.5);
+        vector3.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev3/20*(float)0.5);
     }
 }
