84020544063012c7022af271389626ae9d6fccca
Kristian <krestrepoo@eafit.edu.co>
2024-05-02 20:22:27 -0500

    feat: generate vectors

--- Assets/Scenes/Scripts/VectorRederer.cs
+++ Assets/Scenes/Scripts/VectorRederer.cs
@@ -9,9 +9,24 @@ public class VectorRenderer : MonoBehaviour
     void Start()
     {
         GameManager.instance.v3 = Vector3.Cross(GameManager.instance.v1, GameManager.instance.v2);
-        print(vector1.position);
-        print(vector2.position);
-        print(vector3.position);
+        Quaternion rotation = Quaternion.LookRotation(GameManager.instance.v1);
 
+        vector1.localRotation = rotation;
+        rotation = Quaternion.LookRotation(GameManager.instance.v2);
+        vector2.localRotation = rotation;
+        rotation = Quaternion.LookRotation(GameManager.instance.v3);
+        vector3.localRotation = rotation;
+
+        float magnitudev1 = GameManager.instance.v1.magnitude;
+        float magnitudev2 = GameManager.instance.v2.magnitude;
+        float magnitudev3 = GameManager.instance.v3.magnitude;
+        print(magnitudev1);
+        print(magnitudev2);
+        print(magnitudev3);
+        print(GameManager.instance.v3);
+
+        vector1.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev1/20);
+        vector2.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev2/20);
+        vector3.localScale = new Vector3((float)0.5, (float)0.5, (float)magnitudev3/20);
     }
 }
